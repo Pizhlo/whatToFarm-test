f388b53576b4b8ae204572b516629aad4d89b071
pizhlo <zaush@bk.ru>
2023-05-03 20:29:34 +0300

    edited project structure

--- server/server.go
+++ server/server.go
@@ -1,21 +1,20 @@
-package farm
+package server
 
 import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
-	"github.com/Pizhlo/whatToFarm-test/server"
 )
 
-// Server makes a server with routes and specified port
-func Server(port int) {
+// Start makes a server with routes and specified port
+func Start(port int) {
 	fmt.Println("Launching the server...")
 
 	fmt.Printf("Listening at port %d...\n", port)
 
 	router := gin.Default()
 
-	router.GET("/api/v1/rates", server.GetRates)
+	router.GET("/api/v1/rates", GetRates)
 	//router.POST("/api/v1/rates", server.GetRates)
 
 	router.Run(fmt.Sprintf(":%d", port))
